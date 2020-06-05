66fd51235791d4e944aa0d8c20f86620c0d675c4
tanayraj2603 <45450110+tanayraj2603@users.noreply.github.com>
2020-06-05 11:35:24 +0200

    Add files via upload

---
+++ ledon.ino
@@ -0,0 +1,88 @@
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+#define led 2
+const char* ssid     = "oneplus 5";  
+const char* password = "1121231234";
+
+const char* host     = "LineFollowingRobot.000webhostapp.com"; // Your domain  
+String path          = "/State.json";  
+void setup() {  
+  pinMode(led, OUTPUT); 
+   digitalWrite(led, HIGH);
+   Serial.begin(115200);
+
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  int wifi_ctr = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: " + WiFi.localIP());
+}
+
+void loop() {  
+  Serial.print("connecting to ");
+  Serial.println(host);
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  client.print(String("GET ") + path + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: keep-alive\r\n\r\n");
+
+  delay(500); // wait for server to respond
+
+  // read response
+  String section="header";
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    // Serial.print(line);
+    // we’ll parse the HTML body here
+    if (section=="header") { // headers..
+      Serial.print(".");
+      if (line=="\n") { // skips the empty space at the beginning 
+        section="json";
+      }
+    }
+    else if (section=="json") {  // print the good stuff
+      section="ignore";
+      String result = line.substring(1);
+      
+      // Parse JSON
+      int size = result.length() + 1;
+      char json[size];
+      result.toCharArray(json, size);
+      StaticJsonBuffer<200> jsonBuffer;
+      JsonObject& json_parsed = jsonBuffer.parseObject(json);
+      if (!json_parsed.success())
+      {
+        Serial.println("parseObject() failed");
+        return;
+      }
+
+      // Make the decision to turn off or on the LED
+      if (strcmp(json_parsed["State"], "on") == 0) 
+      {
+        digitalWrite(led, LOW);
+                Serial.println("LED going On");
+      }
+      else if (strcmp(json_parsed["State"], "off") == 0) 
+      {  
+        digitalWrite(led, HIGH);
+                Serial.println("LED turn OFF");
+      }
+            
+    }
+  }
+  Serial.print("closing connection. ");
+}
